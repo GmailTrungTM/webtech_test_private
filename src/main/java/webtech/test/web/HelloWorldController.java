85ba2a77bfad23de6ecc91f4cc8de57199c66684
TrungTM98 <tranminh.t2912@gmail.com>
2022-05-12 12:10:36 +0200

    heroku

--- src/main/java/webtech/test/web/HelloWorldController.java
+++ src/main/java/webtech/test/web/HelloWorldController.java
@@ -3,6 +3,8 @@ package webtech.test.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -10,5 +12,11 @@ public class HelloWorldController {
     @GetMapping(path ="/")
     public ModelAndView showHelloPage() {
         return new ModelAndView("helloworld");
+    }
+
+    @GetMapping("/greeting")
+    public String greeting (@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
